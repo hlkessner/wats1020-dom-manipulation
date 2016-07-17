0822b9df576905f956070b9d6e62a1c82b39ba2e
kessnerh <kessnerh@seattlu.edu>
2016-07-17 19:30:50 -0400

    completed DOM Manipulation

--- js/main.js
+++ js/main.js
@@ -16,32 +16,66 @@ $( document ).ready(function() {
     // Place all your Javascript code inside this "document ready" function so
     // it does not run until the DOM is ready for Javascript manipulation.
 
-    // TODO: Create a function to listen for clicks on the "login" button.
-    //      1. When a user clicks the "login" button, hide the login
-    //          form elements on the page.
-    //      2. Fill the user's first and last name into `div.user-info`.
-    //      (NOTE: You do not have to perform any validation on the data as
-    //          a base requirement.)
+$('.btn-login').on('click',function(){
+  var email=$('.email-input').val();
+  //hides the login form when the user logs into the page
+  $("#login-form").hide();
+  //fills in the users info once they're logged in 
+  $('.user-info').show();
+  $('.user-fullname').html(email);
 
-
-    // TODO: Create a function to listen for clicks on all the "View Details"
+  
+    // A function to listen for clicks on all the "View Details"
     // buttons so that when a user clicks a "View Details" button they see
     // the content contained in the elements with the class "details" in the
     // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
-    //      2. Within that parent, find all the elements that have the class `details`.
-    //      3. Toggle visibility of all the elements within that parent with the class `details`.
-    //      4. Change the text of the "view details" button to read "hide details" so the user
-    //          understands they can hide the text again.
+  
+        $('.view-details').on('click',function(event){
+          console.log(event);
+          var targetElement = event.target;
+          //When user clicks a "view details" button, find the parent of that element.
+          var container= targetElement.parentElement.parentElement;
+          //Within that parent, find all the elements that have the class `details`.
+          $(container).find('.details').each(function (index,el) {
+            //Toggle visibility of all the elements within that parent with the class `details`.
+            if ($(el).is (':visible') ) {
+              $(el).fadeOut();
+              targetElement.innerText = "Veiw Details"
+            } else {
+              $(el).fadeIn();
+              targetElement.innerText = "Hide Details"
+            }
+          });
+        });
 
-    // TODO: Create a function that listens for clicks on the voting buttons and
+    // A function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
     // then determines the updated vote breakdown to adjust the progress bars.
-    //      1. Set up an event listener on the buttons with the `vote` class.
-    //      2. When a button is clicked, look at the `data-vote` attribute to determine
-    //          what the user is voting for ("great" or "greatest").
-    //      3. Increment the counter for whichever vote talley is affected.
-    //      4. Determine the respective percentages (out of 100) for each progress bar.
-    //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
+  
+$('.vote').on('click',function(event){
+  var currentWidth;
+  // When a button is clicked, look at the `data-vote` attribute to determine
+    //what the user is voting for ("great" or "greatest").
+  if ($(event.target).data('vote') == 'great') {
+    //changes the width of the great bar
+    currentWidth= $('.great-progress').width();
+    if (currentWidth + 72 > 720) {
+      currentWidth= 720;
+    } else {
+      currentWidth += 72;
+    }
+    $('.great-progress').width(currentWidth);
+  } else {
+    //changes the width of the greatest bar
+    currentWidth= $('.greatest-progress').width();
+    if (currentWidth + 72 > 720) {
+      currentWidth= 720;
+    } else {
+      currentWidth += 72;
+    }
+    $('.greatest-progress').width(currentWidth);
+  }
 
+});
+});
 });
